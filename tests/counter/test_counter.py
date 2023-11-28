7ead44078f262ddc01d1d5ae4ec84c4d7de0bff2
JRaamos <jhonyramos46@gmail.com>
2023-11-28 12:41:13 -0300

    text: adiciona testes para função counter_ocorrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,17 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    raise
+    "Testa se a função count_ocurrences está funcionando corretamente"
+    assert count_ocurrences("data/jobs.csv", "javascript") == 122
+
+    assert count_ocurrences("data/jobs.csv", "python") == 1639
+
+    "Testa se a contagem de palavras é case insensitive"
+
+    assert count_ocurrences("data/jobs.csv", "Python") == count_ocurrences(
+        "data/jobs.csv", "python"
+    )
+
+    "Testa a contagem de uma palavra que não existe no arquivo"
+    assert count_ocurrences("data/jobs.csv", "inexistente") == 0
