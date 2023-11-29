dd254f9bbc2fee40cefec550fc29d419a62a693b
JRaamos <jhonyramos46@gmail.com>
2023-11-29 12:26:47 -0300

    requisito 06

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -7,7 +7,21 @@ class ProcessSalaries(ProcessJobs):
         super().__init__()
 
     def get_max_salary(self) -> int:
-        pass
+        salaries = self.extract_salaries_from_jobs()
+        max_salary = max(salaries) if salaries else 0
+        return max_salary
+
+    def extract_salaries_from_jobs(self) -> List[int]:
+        salaries = []
+        for job in self.jobs_list:
+            salary_str = job.get("max_salary")
+            if salary_str:
+                try:
+                    salary = int(salary_str)
+                    salaries.append(salary)
+                except ValueError:
+                    continue
+        return salaries
 
     def get_min_salary(self) -> int:
         pass
